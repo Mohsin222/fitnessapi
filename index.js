637db901fe569753d89c5f988bdc7bcc6b2af145
Mohsin222 <mohsinirfan222@gmail.com>
2022-07-16 08:25:44 +0500

    1 images root added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express= require('express')
 const cors= require('cors')
 const port = process.env.PORT || 3000
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 const app= express()
 
@@ -16,6 +18,35 @@ app.get('/',(req,res)=>{
 app.get('/services',(req,res)=>{
     res.send(apiData)
 })
+
+
+var url = "https://unsplash.com/";
+
+const art = [];
+app.get("/my", (req, res) => {
+  axios(url)
+    .then((response) => {
+      const html = response.data;
+      // console.log(html)
+      const $ = cheerio.load(html);
+
+      $(".YVj9w", html).each(function () {
+        const title = $(this).attr("src");
+        //    const url = $(this).attr('href')
+        art.push({
+          title,
+        });
+
+        console.log(art);
+      
+      });
+
+    })
+    .catch((err) => console.log("err"));
+    res.send(art);
+});
+
+
 
 app.listen(port,()=>{
     console.log(`Server running at  ${port}`)
